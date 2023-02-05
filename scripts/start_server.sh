baf187b9ccee245d5aa14076414fc84836d455ef
Rakshit Kumar <32777813+mighty-phoenix@users.noreply.github.com>
2023-02-05 17:00:25 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,8 +4,8 @@ echo starting server
 # here we just use npm to run the build
 cd /var/www/
 echo building application...
-sudo npm run build
+sudo yarn run build
 
 # start the application with pm2
 echo starting application...
-sudo pm2 start npm -- run start
+sudo pm2 start yarn -- run start
