5b7ced6d710bb4b30530ad4d96a4df7f7a1fb809
Rakshit Kumar <32777813+mighty-phoenix@users.noreply.github.com>
2023-02-05 17:30:26 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo starting server
 
+# Setting necessary ENV variables.
+export NEXT_PUBLIC_KELP_TOKEN_ADDRESS=0x91be212c43f7eb84f5f7e34fb5e2cbff4393b4ec
+export NEXT_PUBLIC_CROWD_SALE_ADDRESS=0x76AD551B8ABEFa2Ee9EbD9a70003905D93e81236
+export NEXT_PUBLIC_PROJECT_ID=ab8d50ad2e9c1825f5408b047b76ef24
+export NEXT_PUBLIC_SALE_TYPE=0
+
 # here we just use npm to run the build
 cd /var/www/
 echo building application...
